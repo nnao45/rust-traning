eec2946873797060c4d6e0367ecda8e799d91317
nnao45 <n4sekai5y@gmail.com>
2018-12-23 14:58:26 +0900

    1st

---
+++ hello/target/debug/deps/hello-be4083fd89f28898.d
@@ -0,0 +1,5 @@
+/repo/rust-traning/hello/target/debug/deps/hello-be4083fd89f28898: src/main.rs
+
+/repo/rust-traning/hello/target/debug/deps/hello-be4083fd89f28898.d: src/main.rs
+
+src/main.rs:
---
+++ hello/target/debug/hello.d
@@ -0,0 +1,1 @@
+/repo/rust-traning/hello/target/debug/hello.dSYM: /repo/rust-traning/hello/src/main.rs
