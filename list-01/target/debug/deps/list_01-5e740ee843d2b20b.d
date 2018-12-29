c61ef658a2b1fab6be7f783eec94e6ab27c3cb59
nnao45 <n4sekai5y@gmail.com>
2018-12-29 14:11:14 +0900

    list

---
+++ list-01/target/debug/deps/list_01-5e740ee843d2b20b.d
@@ -0,0 +1,5 @@
+/repo/rust-traning/list-01/target/debug/deps/list_01-5e740ee843d2b20b: src/main.rs
+
+/repo/rust-traning/list-01/target/debug/deps/list_01-5e740ee843d2b20b.d: src/main.rs
+
+src/main.rs:
---
+++ list-01/target/debug/list-01.d
@@ -0,0 +1,1 @@
+/repo/rust-traning/list-01/target/debug/list-01.dSYM: /repo/rust-traning/list-01/src/main.rs
