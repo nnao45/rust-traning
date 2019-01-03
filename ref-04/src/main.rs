c429f9d6ed1f5fed7bd95ba2eba0d60bf211c59e
nnao45 <n4sekai5y@gmail.com>
2019-01-03 15:59:06 +0900

    ref-04

--- ref-04/src/main.rs
+++ ref-04/src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     let x = 10;
     let r = &x;
-    println!("{:?}", r);
+    println!("{:?}", *r); // 実態を見るためには*を使うべき
+    println!("{:?}", r);  // しかし、Rustでは*を使わなくてもref変数に対して暗黙解決する。
 }
